4a634cf17e6f7d66e50839a1debc57199c94efbc
Utku Sarioglu <utkusarioglu@hotmail.com>
2024-07-25 11:22:37 +0000

    Thu Jul 25 11:22:37 UTC 2024

--- scripts/container-script.sh
+++ scripts/container-script.sh
@@ -2,3 +2,4 @@ echo cats > ${GITHUB_WORKSPACE}/file3
 wget -O ${GITHUB_WORKSPACE}/file4 https://www.google.com
 release_info="$(cat /etc/os-release)"
 echo "${release_info}" >> $GITHUB_OUTPUT
+echo "my_name=utku" >> $GITHUB_OUTPUT
