eb1400ee9e26c56abc0a9854b9dabe61904c6810
Utku Sarioglu <utkusarioglu@hotmail.com>
2024-07-25 12:59:09 +0000

    Thu Jul 25 12:59:09 UTC 2024

--- scripts/busybox.sh
+++ scripts/busybox.sh
@@ -5,3 +5,7 @@ echo "command_output='${command_output}'" >> $GITHUB_OUTPUT
 
 echo "Creating secondary_output"
 echo "secondary_output=hello" >> $GITHUB_OUTPUT
+
+echo "Creating local files"
+echo 'hi' > ${GITHUB_WORKSPACE}/file1
+echo 'hello1' > ${GITHUB_WORKSPACE}/file2
