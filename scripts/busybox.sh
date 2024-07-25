e0493e941be0644dfc586dd9dd7be00fede2fc4d
Utku Sarioglu <utkusarioglu@hotmail.com>
2024-07-25 12:14:16 +0000

    Thu Jul 25 12:14:16 UTC 2024

--- scripts/busybox.sh
+++ scripts/busybox.sh
@@ -1,6 +1,6 @@
 command="${1:?First param needs to be the commmand}"
 echo "Running ${command}"
-command_output="$(sh -c $command)"
+command_output="$(sh -c \"$command\")"
 echo "command_output='${command_output}'" >> $GITHUB_OUTPUT
 
 echo "Creating secondary_output"
