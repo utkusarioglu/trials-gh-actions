654e84d7c3e9bb3efc28c9b39db3292cbed3ca61
Utku Sarioglu <utkusarioglu@hotmail.com>
2024-07-25 12:11:04 +0000

    Thu Jul 25 12:11:04 UTC 2024

--- scripts/busybox.sh
+++ scripts/busybox.sh
@@ -3,4 +3,5 @@ echo "Running ${command}"
 command_output="$(sh -c $command)"
 echo "command_output='${command_output}'" >> $GITHUB_OUTPUT
 
+echo "Creating secondary_output"
 echo "secondary_output=hello" >> $GITHUB_OUTPUT
