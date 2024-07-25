12fa97485f65ea53296b8b65f5e25e761071727e
Utku Sarioglu <utkusarioglu@hotmail.com>
2024-07-25 11:30:22 +0000

    Thu Jul 25 11:30:22 UTC 2024

--- scripts/container-script.sh
+++ scripts/container-script.sh
@@ -4,4 +4,4 @@ first_line="$(cat file4 | head -n1)"
 release_info="$(cat /etc/os-release)"
 echo "${release_info}" >> $GITHUB_OUTPUT
 echo "my_name=utku" >> $GITHUB_OUTPUT
-echo "first_line=$first_line" >> $GITHUB_OUTPUT
+echo "first_line='${first_line}'" >> $GITHUB_OUTPUT
