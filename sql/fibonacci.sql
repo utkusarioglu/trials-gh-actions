110d6bfaeede0b9acf6a8745fd649ed7adaea1a0
Utku Sarioglu <utkusarioglu@hotmail.com>
2024-07-27 17:50:45 +0000

    Sat Jul 27 17:50:45 UTC 2024

--- sql/fibonacci.sql
+++ sql/fibonacci.sql
@@ -17,24 +17,41 @@ SELECT
   "num"
 FROM unnest(fibonacci(52)) AS "num";
 
+DECLARE TYPE t_assignment AS (
+  divisor INT,
+  label VARCHAR(4)
+);
+
 CREATE FUNCTION fizzbuzzbar(num INT)
   RETURNS VARCHAR(20)
   LANGUAGE plpgsql
   AS $$
 DECLARE
   val VARCHAR(20) := '';
+  assignments t_assignment[]: [
+    ROW(3, 'fizz')::t_assignment,
+    ROW(5, 'buzz')::t_assignment,
+    ROW(7, 'bar')::t_assignment
+  ];
 BEGIN
-  IF "num" % 3 = 0 THEN
-    val := val || 'fizz';
-  END IF; 
+  FOR a in assignments LOOP
+    IF "num" % a.divisor = 0 THEN
+      val := val || a.label;
+    END IF; 
+  END LOOP;
+  -- IF "num" % 3 = 0 THEN
+  --   val := val || 'fizz';
+  -- END IF; 
 
-  IF "num" % 5 = 0 THEN
-    val := val || 'buzz';
-  END IF; 
+  -- IF "num" % 5 = 0 THEN
+  --   val := val || 'buzz';
+  -- END IF; 
   
-  IF "num" % 7 = 0 THEN
-    val := val || 'bar';
-  END IF; 
+  -- IF "num" % 7 = 0 THEN
+  --   val := val || 'bar';
+  -- END IF; 
+
+  RETURN val;
 END;
 $$;
 
