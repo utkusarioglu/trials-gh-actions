9fb3be35442540efbde07b47e546a66a0d56dbdc
Utku Sarioglu <utkusarioglu@hotmail.com>
2024-07-27 14:05:58 +0000

    Sat Jul 27 14:05:58 UTC 2024

--- sql/fizzbuzz.sql
+++ sql/fizzbuzz.sql
@@ -1,16 +1,19 @@
 CREATE VIEW fizzbuzz AS
-  SELECT 
-    "number",
-    (
-      CASE WHEN "number" % 3 = 0 
-        THEN 'fizz'
-        ELSE ''
-      END
-      ||
-      CASE WHEN "number" % 5 = 0
-        THEN 'buzz'
-        ELSE ''
-      END
-    ) AS "kind"
-  FROM generate_series(1, 200) AS "number"
+  SELECT *
+  FROM (
+    SELECT 
+      "number",
+      (
+        CASE WHEN "number" % 3 = 0 
+          THEN 'fizz'
+          ELSE ''
+        END
+        ||
+        CASE WHEN "number" % 5 = 0
+          THEN 'buzz'
+          ELSE ''
+        END
+      ) AS "kind"
+    FROM generate_series(1, 200) AS "number"
+  )
   WHERE "kind" != '';
