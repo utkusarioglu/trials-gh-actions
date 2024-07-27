7d6e0d2a0d0afb30597368e45b492b13bc6f93cd
Utku Sarioglu <utkusarioglu@hotmail.com>
2024-07-27 17:41:22 +0000

    Sat Jul 27 17:41:22 UTC 2024

--- push.sh
+++ push.sh
@@ -1,3 +1,9 @@
+function error_param {
+  echo "Error: '$2' required as the param number $1"
+}
+
+step_name="${1:?$(error_param 1 'Step Name')}"
+
 function commit {
   git add .
   git commit -m "$(date)"
@@ -5,22 +11,22 @@ function commit {
 }
 
 function wait {
-  sleep_duration=${1:?'First param needs to be the sleep duration'}
+  sleep_duration=${1:?$(error_param 1 'Sleep Duration')}
 
-  echo "Sleeping for ${sleep_duration}…"
+  echo "Sleeping for ${sleep_duration} seconds…"
   sleep $sleep_duration
 }
 
 function get_run_id {
-  gh run list --limit 1 --json databaseId -q ".[0].databaseId"
+  gh run list --limit 1 --json 'databaseId' -q '.[0].databaseId'
 }
 
 
 function log {
-  title=${1:?'First param needs to be the title'}
-  value=${2:?'Second param needs to be the value'}
+  title=${1:?$(error_param 1 'Title')}
+  value=${2:?$(error_param 2 'Value')}
 
-  echo "${title}: ${run_id}"
+  echo "${title}: ${value}"
 }
 
 function gh_watch {
@@ -28,26 +34,33 @@ function gh_watch {
 }
 
 function get_job_id {
-  gh run view "${run_id}" --log --json "jobs" | jq -r '.jobs[].databaseId'
+  run_id=${1:?$(error_param 1 'Run ID')}
+
+  # gh run view "${run_id}" --log --json "jobs" | \
+  #   jq -r '.jobs[].databaseId'
+  gh run view "${run_id}" --log --json 'jobs' -q '.jobs[].databaseId'
+    # jq -r '.jobs[].databaseId'
 }
 
 function get_log {
-  job_id="${1:?'First param needs to be the job id'}"
+  job_id="${1:?$(error_param 1 'Job ID')}"
+  step_name="${2:?$(error_param 2 'Github Step Name')}"
 
+  echo "Showing trimmed logs for '$step_name'…"
   gh run view --job "${job_id}" --log | \
-    grep 'Run Query' | \
-    awk '{$1=""; $2=""; $3=""; $4=""; sub("  ", ""); print}'
+    grep "${step_name}" | \
+    awk '{for(i=1; i < 5; i++) $i=""; sub(/\s{2,}/, ""); print}'
 }
 
 commit
-wait 15
+wait 20
 
-run_id=$(get_run_id)
+run_id="$(get_run_id)"
 log 'Run ID' $run_id
 
 gh_watch
 
-job_id=$(get_job_id)
-log 'Job ID' $job_id
+job_id="$(get_job_id $run_id)"
+log 'Job ID' "${job_id}"
 
-get_log $job_id
+get_log $job_id "${step_name}"
--- sql/fibonacci.sql
+++ sql/fibonacci.sql
@@ -16,3 +16,35 @@ SELECT
   ROW_NUMBER() OVER() AS "row",
   "num"
 FROM unnest(fibonacci(52)) AS "num";
+
+CREATE FUNCTION fizzbuzzbar(num INT)
+  RETURNS VARCHAR(20)
+  LANGUAGE plpgsql
+  AS $$
+DECLARE
+  val VARCHAR(20) := '';
+BEGIN
+  IF "num" % 3 = 0 THEN
+    val := val || 'fizz';
+  END IF; 
+
+  IF "num" % 5 = 0 THEN
+    val := val || 'buzz';
+  END IF; 
+  
+  IF "num" % 7 = 0 THEN
+    val := val || 'bar';
+  END IF; 
+END;
+$$;
+
+CREATE VIEW fizzbuzz AS
+  SELECT * 
+  FROM (
+    SELECT 
+      "number",
+      fizzbuzzbar("number") AS "kind"
+    FROM generate_series(1, 100)
+  )
+  WHERE "kind" != ''
+;
